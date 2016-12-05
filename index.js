e0ee278c6ced3406350ca0900bc08c501386eb4f
Francisco Jordano <fjordano@mozilla.com>
2016-12-05 00:05:51 -1000

    Adding unpnp registration

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const serveStatic = require('serve-static');
 const api = require('./lib/api');
 const app = express();
 const mounter = require('./lib/mounter');
+const package = require('./package.json');
+const MozUnPnP = require('mozunpnp');
 
 app.use(api);
 
@@ -12,6 +14,12 @@ const port = process.env.MIKASA_PORT || 3000;
 // Mount installed apps
 mounter.mountInstalled(app).then(() => {
   debug('Installed apps mounted')
+
+  const mozunpnp = new MozUnPnP();
+  mozunpnp.register('mi-kasa-server', {
+    version: package.version
+  });
+
   app.use('/', express.static('build'));
   app.listen(port, function () {
     debug(`MiKasa server running at ${port}`);
